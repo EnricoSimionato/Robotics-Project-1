9b6ab17117465256c6ebae87e416039e30f37fc4
AlbertoSandri <100241701+AlbertoSandri@users.noreply.github.com>
2022-04-13 13:49:57 +0200

    Added project1 folder

---
+++ project1/src/velocities.cpp
@@ -0,0 +1,85 @@
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#define M_PI 3.14159265358979323846
+
+class VelocityCalculator {
+public:
+  VelocityCalculator() { 
+    this->sub = n.subscribe("wheel_states", 1000, &VelocityCalculator::computeVelocityCallback, this); // mettere lo / in wheel_states?
+  }
+
+  void main_loop() {
+    ros::spin();
+
+  }
+
+  void computeVelocityCallback(const sensor_msgs::JointState::ConstPtr& msg) {
+    
+    if(!this->firstUse)
+    {
+      // The first time initialize ticks and times
+      for(int i = 0; i < sizeof(this->ticks)/sizeof(float); i++)
+        this->ticks[i] = msg->position[i];
+
+      this->times[0] = (msg->header).stamp.sec; //TODO
+      this->times[1] = (msg->header).stamp.nsec;
+
+      this->firstUse = true;
+    }
+    else
+    {
+      float wRuota[4];
+
+      // Calculate the angular velocity of each wheel
+      for(int i = 0; i < sizeof(wRuota)/sizeof(float); i++)
+        wRuota[i] = (2 * M_PI * (msg->position[i] - this->ticks[i])) / 
+            (this->N * this->T * (((msg->header).stamp.sec - this->times[0]) + (((msg->header).stamp.nsec - this->times[1]) / 1000000000)));
+
+      // Calculate the linear velocity v
+      this->v[0] = (this->r * (wRuota[0] + wRuota[1] + wRuota[2] + wRuota[3])) / 4;
+      this->v[1] = (this->r * (- wRuota[0] + wRuota[1] + wRuota[2] - wRuota[3])) / 4;
+      this->v[2] = 0.0;
+      
+      // Calculate the angular velocity w
+      this->w[0] = 0.0;
+      this->w[1] = 0.0;
+      this->w[2] = this->v[0] = (this->r * (- wRuota[0] + wRuota[1] - wRuota[2] + wRuota[3])) / (4 * (this->lX + this->lY));
+      
+      ROS_INFO("w ruota 1: %f", wRuota[0]);
+      ROS_INFO("w ruota 2: %f", wRuota[1]);
+      ROS_INFO("w ruota 3: %f", wRuota[2]);
+      ROS_INFO("w ruota 4: %f", wRuota[3]);
+      ROS_INFO("w ruota 1 reale: %f", msg->velocity[0] * 60);
+      ROS_INFO("w ruota 2 reale: %f", msg->velocity[1] * 60);
+      ROS_INFO("w ruota 3 reale: %f", msg->velocity[2] * 60);
+      ROS_INFO("w ruota 4 reale: %f", msg->velocity[3] * 60);
+      ROS_INFO("v in x: %f", this->v[0]);
+      ROS_INFO("v in y: %f", this->v[1]);
+      ROS_INFO("w in z: %f", this->w[2]);
+    }
+  }
+
+private:
+  ros::NodeHandle n; 
+  ros::Subscriber sub;
+  bool firstUse = false;
+  float ticks[4];
+  int times[2];
+  float v[3];
+  float w[3];
+  int N = 42; // encoder resolution
+  int T = 5; // gear ratio
+  float r = 0.07;
+  float lX = 0.2;
+  float lY = 0.169;
+};
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "velocityCalculator");
+  
+  VelocityCalculator my_velocityCalculator; // constructor is called
+
+  my_velocityCalculator.main_loop();
+
+  return 0;
+}
