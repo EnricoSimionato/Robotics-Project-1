d271fe78559a07a8055213cb96677fc3f9febee2
AlbertoSandri <alberto.sandri@mail.polimi.it>
2022-04-20 11:58:52 +0200

    Added compute control

---
+++ project1/src/control.cpp
@@ -0,0 +1,49 @@
+#include "ros/ros.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "project1/wheels_rpm.h"
+
+class ControlCalculator {
+public:
+  ControlCalculator() { 
+    this->sub = n.subscribe("cmd_vel", 1000, &ControlCalculator::computeControlCallback, this);
+    this->controlPublisher = n.advertise<project1::wheels_rpm>("wheels_rpm", 1000);
+  }
+
+  void main_loop() {
+    ros::spin();
+  }
+
+  void computeControlCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+      project1::wheels_rpm controlMessage;
+      
+      // Calculate the rpm of each wheel
+      controlMessage.rpm_fl = 60 * this->T * ((msg->twist).linear.x - (msg->twist).linear.y - (this->lX + this->lY) * (msg->twist).angular.z) / this->r; 
+      controlMessage.rpm_fr = 60 * this->T * ((msg->twist).linear.x + (msg->twist).linear.y + (this->lX + this->lY) * (msg->twist).angular.z) / this->r; 
+      controlMessage.rpm_rr = 60 * this->T * ((msg->twist).linear.x - (msg->twist).linear.y + (this->lX + this->lY) * (msg->twist).angular.z) / this->r; 
+      controlMessage.rpm_rl = 60 * this->T * ((msg->twist).linear.x + (msg->twist).linear.y - (this->lX + this->lY) * (msg->twist).angular.z) / this->r; 
+      
+      controlMessage.header.stamp.sec = (msg->header).stamp.sec;
+      controlMessage.header.stamp.nsec = (msg->header).stamp.nsec; 
+
+      controlPublisher.publish(controlMessage);
+  }
+
+private:
+  ros::NodeHandle n; 
+  ros::Subscriber sub;
+  ros::Publisher controlPublisher;
+  int T = 5; 
+  float r = 0.07;
+  float lX = 0.2;
+  float lY = 0.169;
+};
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "controlCalculator");
+  
+  ControlCalculator my_controlCalculator;
+
+  my_controlCalculator.main_loop();
+
+  return 0;
+}
