c8bd105c49c13ddca8102f7e23b9f28f9f8aa820
AlbertoSandri <alberto.sandri@mail.polimi.it>
2022-05-07 13:04:52 +0200

    Added ROS parameters and static TF

--- project1/src/odometry.cpp
+++ project1/src/odometry.cpp
@@ -16,8 +16,7 @@ public:
   OdometryCalculator() { 
     this->service = n.advertiseService<project1::Reset::Request, project1::Reset::Response>("reset", boost::bind(&OdometryCalculator::reset_callback, this, pose, _1, _2));
     this->velocitiesSubcriber = n.subscribe("cmd_vel", 1000, &OdometryCalculator::computeOdometryCallback, this); // mettere lo / in wheel_states?
-    this->positionSubscriber = n.subscribe("robot/pose", 1000, &OdometryCalculator::initializePoseCallback, this);
-    this->odometryPublisher = n.advertise<nav_msgs::Odometry>("odom", 1000);
+    this->odometryPublisher = n.advertise<nav_msgs::Odometry>("odom", 1000);   
   }
 
   void main_loop() {
@@ -40,140 +39,129 @@ public:
     return true;
   }
 
+  void computeOdometryCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+    if (this->firstUse)
+    {
+      this->firstUse = false;
+      this->n.getParam("/initial_x", this->pose[0]);
+      this->n.getParam("/initial_y", this->pose[1]);
+      this->n.getParam("/initial_t", this->pose[2]);
+      this->n.getParam("/initial_secs", this->times[0]);
+      this->n.getParam("/initial_nsecs", this->times[1]);
+      this->initialPose[0] = this->pose[0];
+      this->initialPose[1] = this->pose[1];
+      this->initialPose[2] = this->pose[2];
+    }
+    else
+    {
+      float ts = (msg->header).stamp.sec - this->times[0] + ((float) (msg->header).stamp.nsec) / 1000000000.0 - ((float) this->times[1]) / 1000000000.0;
 
-  void initializePoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-      if(!this->firstUse) {
-        this->times[0] = (msg->header).stamp.sec;
-        this->times[1] = (msg->header).stamp.nsec;
+      this->pose[2] = this->pose[2] + (msg->twist).angular.z * ts;
 
-        this->pose[0] = msg->pose.position.x;
-        this->pose[1] = msg->pose.position.y;
-        this->initialPose[0] = msg->pose.position.x;
-        this->initialPose[1] = msg->pose.position.y;
+      float v = sqrt((msg->twist).linear.x * (msg->twist).linear.x + (msg->twist).linear.y * (msg->twist).linear.y);
+      
+      float teta;
+      if ((msg->twist).linear.x == 0.0) 
+        (msg->twist).linear.y >= 0.0 ? teta = M_PI / 2.0 : teta = - M_PI / 2.0;
+      else if ((msg->twist).linear.x > 0.0)
+        teta = atan((msg->twist).linear.y / (msg->twist).linear.x);
+      else teta = atan((msg->twist).linear.y / (msg->twist).linear.x) + M_PI;
 
-        tf2::Quaternion q(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w);
-        double roll, pitch, yaw;
-        tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
+      if(this->integrationMode == 0) {
 
-     
-
-        this->pose[2] = yaw;
-        this->initialPose[2] = yaw;
-        /*
+        this->pose[0] = this->pose[0] + v * cos(this->pose[2] + teta) * ts;
+        this->pose[1] = this->pose[1] + v * sin(this->pose[2] + teta) * ts;
+        /*ROS_INFO("Euler method");  
+        ROS_INFO("position in x: %f", this->pose[0]);
         ROS_INFO("position in x: %f", this->pose[0]);
         ROS_INFO("position in y: %f", this->pose[1]);
         ROS_INFO("orientation: %f", this->pose[2]);
-        */
+      */
+      } else if (this->integrationMode == 1) {
 
-        this->firstUse = true;
+        this->pose[0] = this->pose[0] + v * cos(this->pose[2] + teta + (msg->twist).angular.z * ts / 2) * ts;
+        this->pose[1] = this->pose[1] + v * sin(this->pose[2] + teta + (msg->twist).angular.z * ts / 2) * ts;
+  /*
+        ROS_INFO("RK method");
+        ROS_INFO("position in x: %f", this->pose[0]);
+        ROS_INFO("position in x: %f", this->pose[0]);
+        ROS_INFO("position in y: %f", this->pose[1]);
+        ROS_INFO("orientation: %f", this->pose[2]);
+    */  
+      } else {
+        ROS_INFO("Error");
       }
-  }
 
-  void computeOdometryCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
-    float ts = (msg->header).stamp.sec - this->times[0] + ((float) (msg->header).stamp.nsec) / 1000000000.0 - ((float) this->times[1]) / 1000000000.0;
+        this->times[0] = (msg->header).stamp.sec;
+        this->times[1] = (msg->header).stamp.nsec;
+        
+        nav_msgs::Odometry odometryMessage;
 
-    this->pose[2] = this->pose[2] + (msg->twist).angular.z * ts;
+        // x,y position
+        odometryMessage.pose.pose.position.x = this->pose[0];
+        odometryMessage.pose.pose.position.y = this->pose[1];
+        odometryMessage.pose.pose.position.z = 0.0;
+        
+        // orientation
+        tf2::Quaternion q;
+        q.setRPY(0, 0, this->pose[2]); //this should be in radians
+        odometryMessage.pose.pose.orientation.x = q.x();
+        odometryMessage.pose.pose.orientation.y = q.y();
+        odometryMessage.pose.pose.orientation.z = q.z();
+        odometryMessage.pose.pose.orientation.w = q.w();
 
-    float v = sqrt((msg->twist).linear.x * (msg->twist).linear.x + (msg->twist).linear.y * (msg->twist).linear.y);
-    
-    float teta;
-    if ((msg->twist).linear.x == 0.0) 
-      (msg->twist).linear.y >= 0.0 ? teta = M_PI / 2.0 : teta = - M_PI / 2.0;
-    else if ((msg->twist).linear.x > 0.0)
-      teta = atan((msg->twist).linear.y / (msg->twist).linear.x);
-    else teta = atan((msg->twist).linear.y / (msg->twist).linear.x) + M_PI;
+        // v,w velocities
+        odometryMessage.twist.twist.linear.x = (msg->twist).linear.x;
+        odometryMessage.twist.twist.linear.y = (msg->twist).linear.y;
+        odometryMessage.twist.twist.linear.z = (msg->twist).linear.z;
+        odometryMessage.twist.twist.angular.x = (msg->twist).angular.x;
+        odometryMessage.twist.twist.angular.y = (msg->twist).angular.y;
+        odometryMessage.twist.twist.angular.z = (msg->twist).angular.z;
 
-    if(this->integrationMode == 0) {
+        // time
+        odometryMessage.header.stamp.sec = this->times[0];
+        odometryMessage.header.stamp.nsec = this->times[1];
+        odometryMessage.header.frame_id = "odom"; 
+        odometryMessage.child_frame_id = "base_link"; 
+        odometryPublisher.publish(odometryMessage);   
 
-      this->pose[0] = this->pose[0] + v * cos(this->pose[2] + teta) * ts;
-      this->pose[1] = this->pose[1] + v * sin(this->pose[2] + teta) * ts;
-      /*ROS_INFO("Euler method");  
-      ROS_INFO("position in x: %f", this->pose[0]);
-      ROS_INFO("position in x: %f", this->pose[0]);
-      ROS_INFO("position in y: %f", this->pose[1]);
-      ROS_INFO("orientation: %f", this->pose[2]);
-    */
-    } else if (this->integrationMode == 1) {
 
-      this->pose[0] = this->pose[0] + v * cos(this->pose[2] + teta + (msg->twist).angular.z * ts / 2) * ts;
-      this->pose[1] = this->pose[1] + v * sin(this->pose[2] + teta + (msg->twist).angular.z * ts / 2) * ts;
-/*
-      ROS_INFO("RK method");
-      ROS_INFO("position in x: %f", this->pose[0]);
-      ROS_INFO("position in x: %f", this->pose[0]);
-      ROS_INFO("position in y: %f", this->pose[1]);
-      ROS_INFO("orientation: %f", this->pose[2]);
-  */  
-    } else {
-      ROS_INFO("Error");
+
+      /*  transformStamped.header.frame_id = "world"; 
+        transformStamped.child_frame_id = "odom"; 
+   
+        transformStamped.transform.translation.x = this->initialPose[0];
+        transformStamped.transform.translation.y = this->initialPose[1];
+        transformStamped.transform.translation.z = 0.0;
+      
+        tf2::Quaternion q1; 
+        q1.setRPY(0, 0, this->initialPose[2]);
+        transformStamped.transform.rotation.x = q1.x();
+        transformStamped.transform.rotation.y = q1.y();
+        transformStamped.transform.rotation.z = q1.z();
+        transformStamped.transform.rotation.w = q1.w();
+
+        br.sendTransform(transformStamped);*/
+
+
+        transformStamped.header.frame_id = "odom"; 
+        transformStamped.child_frame_id = "base_link"; 
+   
+        transformStamped.transform.translation.x = this->pose[0] - this->initialPose[0];
+        transformStamped.transform.translation.y = this->pose[1] - this->initialPose[1];
+        transformStamped.transform.translation.z = 0.0;
+       
+        tf2::Quaternion q1; 
+        q1.setRPY(0, 0, this->pose[2] - this->initialPose[2]);
+        transformStamped.transform.rotation.x = q1.x();
+        transformStamped.transform.rotation.y = q1.y();
+        transformStamped.transform.rotation.z = q1.z();
+        transformStamped.transform.rotation.w = q1.w();
+
+        br.sendTransform(transformStamped);   
+
     }
-
-      this->times[0] = (msg->header).stamp.sec;
-      this->times[1] = (msg->header).stamp.nsec;
-      
-      nav_msgs::Odometry odometryMessage;
-
-      // x,y position
-      odometryMessage.pose.pose.position.x = this->pose[0];
-      odometryMessage.pose.pose.position.y = this->pose[1];
-      odometryMessage.pose.pose.position.z = 0.0;
-      
-      // orientation
-      tf2::Quaternion q;
-      q.setRPY(0, 0, this->pose[2]); //this should be in radians
-      odometryMessage.pose.pose.orientation.x = q.x();
-      odometryMessage.pose.pose.orientation.y = q.y();
-      odometryMessage.pose.pose.orientation.z = q.z();
-      odometryMessage.pose.pose.orientation.w = q.w();
-
-      // v,w velocities
-      odometryMessage.twist.twist.linear.x = (msg->twist).linear.x;
-      odometryMessage.twist.twist.linear.y = (msg->twist).linear.y;
-      odometryMessage.twist.twist.linear.z = (msg->twist).linear.z;
-      odometryMessage.twist.twist.angular.x = (msg->twist).angular.x;
-      odometryMessage.twist.twist.angular.y = (msg->twist).angular.y;
-      odometryMessage.twist.twist.angular.z = (msg->twist).angular.z;
-
-      // time
-      odometryMessage.header.stamp.sec = this->times[0];
-      odometryMessage.header.stamp.nsec = this->times[1];
-      odometryMessage.header.frame_id = "world"; 
-      odometryMessage.child_frame_id = "base_link"; 
-      odometryPublisher.publish(odometryMessage);   
-
-
-
-      transformStamped.header.frame_id = "world"; 
-      transformStamped.child_frame_id = "odom"; 
- 
-      transformStamped.transform.translation.x = this->initialPose[0];
-      transformStamped.transform.translation.y = this->initialPose[1];
-      transformStamped.transform.translation.z = 0.0;
-    
-      tf2::Quaternion q1; 
-      q1.setRPY(0, 0, this->initialPose[2]);
-      transformStamped.transform.rotation.x = q1.x();
-      transformStamped.transform.rotation.y = q1.y();
-      transformStamped.transform.rotation.z = q1.z();
-      transformStamped.transform.rotation.w = q1.w();
-
-      br.sendTransform(transformStamped);
-
-
-      transformStamped.header.frame_id = "odom"; 
-      transformStamped.child_frame_id = "base_link"; 
- 
-      transformStamped.transform.translation.x = this->pose[0] - this->initialPose[0];
-      transformStamped.transform.translation.y = this->pose[1] - this->initialPose[1];
-      transformStamped.transform.translation.z = 0.0;
      
-      q1.setRPY(0, 0, this->pose[2] - this->initialPose[2]);
-      transformStamped.transform.rotation.x = q1.x();
-      transformStamped.transform.rotation.y = q1.y();
-      transformStamped.transform.rotation.z = q1.z();
-      transformStamped.transform.rotation.w = q1.w();
-
-      br.sendTransform(transformStamped);   
   }
 
   void setIntegrationMode(int integrationMethod) {
@@ -188,11 +176,11 @@ private:
   ros::ServiceServer service;
   tf2_ros::TransformBroadcaster br;
   geometry_msgs::TransformStamped transformStamped;
-  bool firstUse = false;
   int times[2];
   float pose[3];
   float initialPose[3];
   int integrationMode;
+  bool firstUse = true;
 };
 
 void integrationMethodCallback(OdometryCalculator *my_odometryCalculator, project1::parametersConfig &config, uint32_t level) {
